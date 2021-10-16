3e72fd8de434b8917b159cd08a2a6e4af39d54f1
Giancarlos Blanco <giancarlosbv@gmail.com>
2021-10-16 13:00:26 -0400

    commit

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CardList from './CardList.jsx';
+import UnitContainer from './UnitContainer.jsx';
 
 const App = () => {
   return (
     <div>
       <h1>App.js is connected and working!</h1>
+      <UnitContainer/>
       <CardList />
     </div>
   )
--- client/src/components/CardList.jsx
+++ client/src/components/CardList.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import cards from '../cards.js';
+import { useState } from 'react';
 //pull request example
 var CardList = () => {
+
+  const [cost,setCost] = useState(0);
+  const [attack,setAttack] = useState(0);
+  const [health,setHealth] = useState(0);
+  
+
 
   const levelCheck = (type, supertype, rarity, name) => {
     if (type === 'Unit' && supertype === 'Champion' && rarity === 'None') {
@@ -12,19 +19,25 @@ var CardList = () => {
     }
   }
 
+
+ 
+
   return (
   <ul className="cards">
     {cards().map((card) => {
       return (
-      <li key={card.cardCode}>
+      <li key={card.cardCode}
+      className = {`${card.type}container`}>
         <div className = {card.type}>
           <img
           className = {card.type}
           src = {`./cards/${card.cardCode}-full.png`} />
         </div>
+      {/* level check for champion level up */}
         <p>
           {levelCheck(card.type, card.supertype, card.rarity, card.name)}
         </p>
+        
       </li>
       )}
     )}
