64ae7d154d1b408dfff6d6cd20d96e83b4f4fe8b
Giancarlos Blanco <giancarlosbv@gmail.com>
2021-10-16 16:37:49 -0400

    filterCheck created

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import CardList from './CardList.jsx';
+import RegionFiltered from './RegionFiltered.js';
 import UnitContainer from './UnitContainer.jsx';
 
 const App = () => {
   return (
     <div>
       <h1>App.js is connected and working!</h1>
-      <UnitContainer/>
+      {/* <UnitContainer/> */}
       <CardList />
+      {/* <RegionFiltered/> */}
     </div>
   )
 }
